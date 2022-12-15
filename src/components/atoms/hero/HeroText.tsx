712feba0130ca7d553afecd2694c94717692b5a4
Ondrej Salek <57462486+ondrasalek@users.noreply.github.com>
2022-12-15 11:24:19 +0100

    creating hero section

---
+++ src/components/atoms/hero/HeroText.tsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+const HeroText = styled.h1`
+    width:100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    text-align: center;
+    color: white;
+    font-size: 3em;
+
+`;
+
+export default HeroText;
