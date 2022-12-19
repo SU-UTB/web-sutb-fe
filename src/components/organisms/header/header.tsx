895507a1077d6755596ef79a7632e5d84d24c0e6
Ondrej Salek <ondrasalek@gmail.com>
2022-12-19 14:32:20 +0100

    move assets folder

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -2,21 +2,20 @@ import Title from '../../atoms/header/Title';
 import Logo from '../../atoms/header/Logo';
 import User from '../../atoms/header/User';
 
+import SULogoSVG from '../../../assets/images/SULogo.svg';
+import PersonSVG from '../../../assets/images/person.svg';
 import { Wrapper } from './style';
+import IHeaderProps from '../../../lib/interfaces/IHeaderProps';
 
-interface HeaderProps {
-    title: string;
-}
-
-const Header = ({ title }: HeaderProps) => {
+const Header = ({ title }: IHeaderProps) => {
     return (
         <Wrapper>
             <a href="/">
-                <Logo src="/suLogo.svg" alt="Logo" />
+                <Logo src={SULogoSVG} alt="Logo" />
             </a>
             <Title>{title}</Title>
             <a href="/login">
-                <User src="/person.svg" alt="User" />
+                <User src={PersonSVG} alt="User" />
             </a>
 
         </Wrapper>
