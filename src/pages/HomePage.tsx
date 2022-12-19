bd6f6fd7da1640abf6751a45c85117bf72d43a6c
Ondrej Salek <ondrasalek@gmail.com>
2022-12-19 15:06:42 +0100

    type of object added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,15 +2,24 @@ import Template from '../components/templates/HomeTemplate';
 
 const HomePage = () => {
     document.title = "Domů - Studentská unie UTB";
-
     const comingSoon: string = "https://pruvodcestudenta.utb.cz/";
-
-    type Partner = [string, string, string];
-    const partners: Partner[] =
-        [
-            ["./assets/utb.svg", "Univerzita Tomáše Bati", "https://www.utb.cz/"],
-            ["./assets/kmz-cs.svg", "Koleje a Menza", "https://kmz.utb.cz/"],
-        ]
+    type Partner = {
+        src: string;
+        alt: string;
+        href: string;
+    };
+    const partners: Partner[] = [
+        {
+            src: "./assets/images/utb.svg",
+            alt: "Univerzita Tomáše Bati",
+            href: "https://www.utb.cz/",
+        },
+        {
+            src: "./assets/images/kmz-cs.svg",
+            alt: "Koleje a Menza",
+            href: "https://kmz.utb.cz/",
+        },
+    ];
     return (
         <Template
             comingSoon={comingSoon}
