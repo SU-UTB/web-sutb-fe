a1265dd13814401b62cc8b99a4eb4fb12a3504e6
Ondrej Salek <57462486+ondrasalek@users.noreply.github.com>
2023-02-28 00:09:49 +0100

    fixed logo svg

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,13 +1,13 @@
 import { IoClose, IoMenu } from 'react-icons/io5';
 import { Logo, MenuButton, Title, Wrapper } from './Header.styled';
-import { useEffect, useState } from 'react';
 
 import BaseLink from '../../shared/BaseLink';
 import IHeaderProps from '../../../lib/interfaces/IHeaderProps';
 import Navbar from './navbar/Navbar';
-import SULogoSymbol from '../../../assets/images/SU_logo_symbol_05_RGB.png';
-import SULogoText from '../../../assets/images/SU_logo_sirka_05_RGB.png';
+import SULogoSVG from '../../../assets/images/suLogo.svg';
+import SULogoText from '../../../assets/images/SU_logo_sirka_04_RGB.png';
 import { isMobile } from 'react-device-detect';
+import { useState } from 'react';
 
 const Header = ({ title }: IHeaderProps) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +18,7 @@ const Header = ({ title }: IHeaderProps) => {
         <Wrapper>
             <BaseLink href="/">
                 {!isMobile && <Logo src={SULogoText} alt="Logo s textem" size={200} />}
-                {isMobile && <Logo src={SULogoSymbol} alt="Logo symbol" size={50} />}
+                {isMobile && <Logo src={SULogoSVG} alt="Logo symbol" size={50} />}
             </BaseLink>
             {isMobile && <Title>{title}</Title>}
             {isMobile &&
