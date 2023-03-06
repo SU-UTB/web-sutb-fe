07363d507fa6c9fe7e7d416b8c05f672b715bc38
Ondrej Salek <57462486+ondrasalek@users.noreply.github.com>
2023-03-06 16:02:17 +0100

    paddings edit

--- src/components/articles/Article.styled.tsx
+++ src/components/articles/Article.styled.tsx
@@ -6,10 +6,11 @@ export const Wrapper = styled.article<{
     ({ primary }) => css`
         display: flex;
         flex-direction: column;
+        gap: 20px;
         ${primary && css`
-            margin: 0 20px;
+        margin: 0 20px 20px;
         `}
         @media only screen and (min-width: ${({ theme }) => theme.breakpoints.mobile}) {
-            margin: 0 12%;
+            margin: 0 12% 20px;
         }
 `);
