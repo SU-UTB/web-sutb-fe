992e906d62ef076094d0ff6d0a578602bf29620f
Ondrej Salek <57462486+ondrasalek@users.noreply.github.com>
2023-03-22 15:31:43 +0100

    meta tags using Helmet

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,19 +3,33 @@ import { useEffect, useState } from 'react';
 
 import { CookiesProvider } from '../../context/cookies/CookiesContext';
 import Footer from './footer/Footer';
+import HeadHelmet from '../head/HeadHelmet';
 import Header from './header/Header';
 import { Outlet } from 'react-router-dom';
+import { ROUTE_PATHS } from '../../lib/constants/Routes';
+import { useLocation } from 'react-router-dom';
 
 const Layout = () => {
     const [title, setTitle] = useState('');
+    const location = useLocation();
+
     useEffect(() => {
-        setTitle(() => {
-            const parts = document.title.split('-');
-            return parts[0].trim();
-        })
-    }, []);
+        switch (location.pathname) {
+            case ROUTE_PATHS.HOME.linkTo:
+                setTitle('Domů');
+                break;
+
+            case ROUTE_PATHS.ABOUT.linkTo:
+                setTitle('O nás');
+                break;
+            case ROUTE_PATHS.CONTACT.linkTo:
+                setTitle('Kontakt');
+                break;
+        }
+    }, [location.pathname]);
     return (
         <CookiesProvider>
+            <HeadHelmet title={title} />
             <Wrapper>
                 <Header title={title} />
                 <Main>
